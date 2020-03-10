675c1c13d145d65907e28acc97f5844762caea3c
taithanh95 <58700144+taithanh95@users.noreply.github.com>
2020-03-11 01:25:16 +0700

    Update Main.java

--- src/com/bof/file/main/Main.java
+++ src/com/bof/file/main/Main.java
@@ -1,10 +1,19 @@
 package com.bof.file.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.bof.file.FileMng;
 
 public class Main {
 	public static void main(String[] args) {
-		FileMng test = new FileMng("E:/avbcetrtrtrtrtrtr.txt");
-		test.readFile();
+		FileMng test = new FileMng("E:/data_bai_1.txt");
+		List<String> listDataList = new ArrayList<String>();
+		listDataList.add("1");
+		listDataList.add("2");
+		listDataList.add("9");
+		listDataList.add("4");
+		listDataList.add("7");
+		test.sortListDataAsc(listDataList);
 	}
 }
